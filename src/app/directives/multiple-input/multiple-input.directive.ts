e5c739d721b0429e8a2d7ce66d66bf9d2c76ae9a
eduardopacreu <eduardomario100@gmail.com>
2020-02-11 20:22:45 -0600

    Se modifica directiva para actuar como un solo input

--- src/app/directives/multiple-input/multiple-input.directive.ts
+++ src/app/directives/multiple-input/multiple-input.directive.ts
@@ -7,6 +7,7 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 export class MultipleInputDirective {
 
   element: any;
+  position: any;
   @Input('multiple-input') limit: string;
   constructor(
     private elementRef: ElementRef
@@ -15,21 +16,26 @@ export class MultipleInputDirective {
   }
 
   @HostListener('input', ['$event']) onInputChange(event) {
-    console.log(event);
     const initalValue = this.element.value;
+    console.log('Event', event);
+    console.log('Initial', initalValue);
+    console.log('Initial', initalValue.split(''));
     switch (event.inputType) {
       case 'deleteContentBackward':
-        const array = (this.element.value).replace('\t', '').split('');
-        if (array[array.length - 1] !== '\t') {
-          const lastString = (this.element.value).substring(0, (this.element.value).length - 1);
-          this.element.value = this.createValue(lastString);
-        }
+        this.element.value = this.createValue(this.deleteValue(initalValue));
+        setTimeout(() => {
+          event.target.setSelectionRange(0, 0);
+        }, 1);
         break;
       default:
-        const noWordString = (this.element.value).replace(/[^0-9]*/g, '');
+        const noWordString = this.element.value.replace(/[^0-9]*/g, '');
         this.element.value = this.createValue(noWordString);
         break;
     }
+    console.log(this.elementRef);
+    console.log(this.element.children);
+    console.log('Final', this.element.value);
+    console.log('Final', this.element.value.replace(/\t/g, ''));
     if (initalValue !== this.element.value) {
       event.stopPropagation();
     }
@@ -38,11 +44,26 @@ export class MultipleInputDirective {
   createValue(value: string) {
     let newValue = '';
     let actualLimit = 1;
-    for (const char of (value).replace('\t', '').split('')) {
-      if (actualLimit === parseInt(this.limit, 10)) {
+    for (const char of value.replace(/\t/g, '').split('')) {
+      if (actualLimit === parseInt(this.limit, 10) || actualLimit === value.replace(/\t/g, '').split('').length) {
         newValue = newValue + char;
-      } else if (char !== '\t') {
-        newValue = newValue + `${char}\t`;
+      } else if (actualLimit < parseInt(this.limit, 10)) {
+        newValue = newValue + char + '\t';
+      }
+      actualLimit++;
+    }
+    return newValue;
+  }
+
+  deleteValue(value: string) {
+    let newValue = '';
+    let actualLimit = 1;
+    for (const char of value.split('\t')) {
+      if (char.length > 1) {
+        newValue = newValue + char.substring(1);
+        this.position = actualLimit - 1;
+      } else {
+        newValue = newValue + char;
       }
       actualLimit++;
     }
